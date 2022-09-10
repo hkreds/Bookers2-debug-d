773af39cb1a1ebcb9c8f0d1a6611db3dc8ea5aa0
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-10 01:20:39 +0000

    [Start]課題9d モデルへの記述

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,12 +2,13 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :book_tags, dependent: :destroy
+  has_many :tags, through: :book_tags
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
   
   scope :latest, -> {order(created_at: :desc)}
-
   scope :star_count, -> {order(star: :desc)}
   
   def favorited_by?(user)
