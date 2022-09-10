9c61c8fb2cb5cc1813a29aaebb982a65a17b19ec
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-10 02:36:46 +0000

    [Add]課題9d viewにタグ入力フォーム

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    tag_list=params[:book][:tag_name].split(',')
     if @book.save
+      @book.save_tags(tag_list)
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
@@ -57,6 +59,6 @@ class BooksController < ApplicationController
 
   private
     def book_params
-      params.require(:book).permit(:title, :body, :star, :tag)
+      params.require(:book).permit(:title, :body, :star, :tag_name)
     end
 end
