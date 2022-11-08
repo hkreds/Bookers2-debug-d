067666002d6fb0db0e0f32393bd42c8242c0d625
hkreds <109258421+hkreds@users.noreply.github.com>
2022-11-08 07:01:03 +0000

    [Add]bookにtagカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_10_010830) do
+ActiveRecord::Schema.define(version: 2022_11_08_065700) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_09_10_010830) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "star"
+    t.string "tag"
   end
 
   create_table "favorites", force: :cascade do |t|
